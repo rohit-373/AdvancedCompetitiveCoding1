9a24d681d5158bbaf29a7107c37646053688a3a7
Rohit Elamurugan <48488024+rohit-373@users.noreply.github.com>
2025-08-25 20:27:24 +0530

    made changes on todays class

--- EuclidAlgorithm.java
+++ EuclidAlgorithm.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 public class EuclidAlgorithm {
-public int gcd(int a, int b) {
-if (a == 0) return b;
-return gcd(b%a, a);
-}
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int a = sc.nextInt();
-int b = sc.nextInt();
-int ans = gcd(a, b);
-System.out.println(ans);
-}
+    static int gcd(int a, int b) {
+        if (a == 0)
+            return b;
+        return gcd(b%a, a);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int ans = gcd(a, b);
+        System.out.println(ans);
+    }
 }
