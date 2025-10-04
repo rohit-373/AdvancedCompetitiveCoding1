ae683b676bb29f31dfb1c0ff7ea65fbf96e0cf4b
Rohit Elamurugan <rohitelamurugan1607@gmail.com>
2025-10-04 12:29:22 +0000

    new code

---
+++ BoothsAlgorithm.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class BoothsAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int multiplicand = sc.nextInt();
+        int multiplier = sc.nextInt();
+        int product = 0;
+        int multiplicandBits = Integer.toBinaryString(multiplicand).length();
+        for (int i = 0; i < multiplicandBits; i++) {
+            int currentBit = multiplicand & 1;
+            if (currentBit == 1) {
+                product += multiplier;
+            }
+            multiplier <<= 1;
+            multiplicand >>>= 1;
+        }
+        System.out.println(product);
+    }
+}
